7aff5e1ce31242bf762f87a25b81ff85f385fb59
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-09-02 16:31:42 -0400

    update a bunch of figs

--- assets/cluster_overview.py
+++ assets/cluster_overview.py
@@ -29,5 +29,11 @@ hcp.text(54, 5.1, 'W51', color='white', ha='center', va='bottom')
 hcp.plot(30, 6.0, 'o', color='green', markersize=10, markerfacecolor='none', zorder=50, alpha=1, markeredgewidth=2, markeredgecolor='green')
 hcp.text(34, 6.0, 'W43', color='white', ha='center', va='bottom')
 
+ax.axis['bottom'].major_ticklabels.set_visible(False)
+ax.axis['left'].major_ticklabels.set_visible(False)
+
+
 pl.draw()
 pl.savefig("galaxyoverview_clusters_and_protoclusters.png", bbox_inches='tight')
+ax.axis([-10, 10, -10, 10])
+pl.savefig("galaxyoverview_clusters_and_protoclusters_zoom.png", bbox_inches='tight')

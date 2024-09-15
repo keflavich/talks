c201056a1adcee128f46ad3551a389a27ab46fc4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-09-15 14:02:25 -0400

    add new galaxy overview figure, update summary

--- assets/cluster_overview.py
+++ assets/cluster_overview.py
@@ -1,4 +1,5 @@
 import pylab as pl
+from astropy import coordinates, units as u
 from milkywayplots.milkywayplot import make_mw_plot, get_image
 
 mw_img_name = get_image()
@@ -34,9 +35,9 @@ ax.axis['left'].major_ticklabels.set_visible(False)
 
 
 pl.draw()
-pl.savefig("galaxyoverview_clusters_and_protoclusters.png", bbox_inches='tight')
+pl.savefig("galaxyoverview_clusters_and_protoclusters.png", bbox_inches='tight', dpi=150)
 ax.axis([-10, 10, -10, 10])
-pl.savefig("galaxyoverview_clusters_and_protoclusters_zoom.png", bbox_inches='tight')
+pl.savefig("galaxyoverview_clusters_and_protoclusters_zoom.png", bbox_inches='tight', dpi=150)
 
 for elt in hcp.texts + gcp.texts + gcp.lines + hcp.lines:
     elt.set_visible(False)
@@ -54,4 +55,32 @@ circ = pl.matplotlib.patches.Circle([0, 8.5], 1)
 circ.set_edgecolor('red')
 circ.set_facecolor((1, 0, 0, 0.25))
 ax.add_artist(circ)
-pl.savefig("galaxyoverview_local_kiloparsec.png", bbox_inches='tight', edgecolor='none', facecolor='none')
+pl.savefig("galaxyoverview_local_kiloparsec.png", bbox_inches='tight', edgecolor='none', facecolor='none', dpi=150)
+
+circ.set_visible(False)
+
+# see also allskylabeledregions.py
+
+saltdiskcoords = {
+    'W33': coordinates.SkyCoord.from_name('W33'),
+    'NGC6334': coordinates.SkyCoord.from_name('NGC6334'),
+    'G17': coordinates.SkyCoord(17.64*u.deg, 0.16*u.deg, frame='galactic'),
+    'G351': coordinates.SkyCoord(351.77*u.deg, -0.51*u.deg, frame='galactic'),
+    'I16547': coordinates.SkyCoord.from_name('IRAS 16547-4247'),
+    'SrcI': coordinates.SkyCoord.from_name("Orion Source I"),
+}
+saltdiskdistances = {
+    'W33': 2.6,
+    'NGC6334': 1.3,
+    'G17': 2.2,
+    'G351': 2.2,
+    'I16547': 2.9,
+    'SrcI': 0.4,
+}
+
+for sdname in saltdiskcoords:
+    crd = saltdiskcoords[sdname]
+    hcp.plot(crd.galactic.l.deg, saltdiskdistances[sdname], 'o', color='green', markersize=5, markerfacecolor='none', zorder=50, alpha=1, markeredgewidth=2, markeredgecolor='green')
+    hcp.text(crd.galactic.l.deg, saltdiskdistances[sdname], sdname, color='white', ha='center', va='bottom', zorder=55)
+
+pl.savefig("galaxyoverview_saltdisks.png", bbox_inches='tight', edgecolor='none', facecolor='none', dpi=150)
